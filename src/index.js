704b0fc4531c8c0d6058edd4796b997885b5a383
NikitaT1 <nik.tati1@gmail.com>
2021-04-29 11:29:07 +0300

    Updating app for deploying

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 import { BrowserRouter } from "react-router-dom";
 
+// "apiUrl": "http://localhost:3900/api/"
 ReactDOM.render(
   <BrowserRouter>
     <App />
--- src/store/middleware/api.js
+++ src/store/middleware/api.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import * as actions from "../api";
 import config from "../../config.json";
 
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+
 const api = ({ dispatch }) => (next) => async (action) => {
   if (action.type !== actions.apiCallBegan.type) return next(action);
 
@@ -11,43 +13,7 @@ const api = ({ dispatch }) => (next) => async (action) => {
 
   next(action);
 
-  // switch (action.payload.onType) {
-  //   case "loadBooks":
-  //     try {
-  //       const response = await getBooks();
-  //       dispatch(actions.apiCallSuccess());
-  //       if (onSuccess) dispatch({ type: onSuccess, payload: response });
-  //       return;
-  //     } catch (error) {
-  //       dispatch(actions.apiCallFailed(error.message));
-  //     }
-
-  //   case "bookDelete":
-  //     debugger;
-  //     try {
-  //       const response = await deleteBook(data);
-  //       dispatch(actions.apiCallSuccess());
-  //       if (onSuccess) dispatch({ type: onSuccess, payload: response });
-  //     } catch (error) {
-  //       dispatch(actions.apiCallFailed(error.message));
-  //     }
-  // }
-
-  // try {
-  //   const response = await getBooks();
-  //   dispatch(actions.apiCallSuccess());
-  //   if (onSuccess) dispatch({ type: onSuccess, payload: response });
-  // } catch (error) {
-  //   dispatch(actions.apiCallFailed(error.message));
-  // }
-
   try {
-    // let response;
-    // if (method === "DELETE")
-    //   response = await axios.delete(config.apiUrl + url + data);
-    // dispatch(actions.apiCallSuccess(response.data));
-    // if (onSuccess) dispatch({ type: onSuccess, payload: response.data });
-    // else
     const response = await axios.request({
       baseURL: config.apiUrl,
       url,
